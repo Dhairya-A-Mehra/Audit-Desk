fe996b91ff97f9670c6f2966742a9d3fedb251e0
Dhairya Mehra <mpdhairyamehra000942@gmail.com>
2024-03-29 10:34:18 +0530

    Update TableEntries.sql

--- AuditDesk/Database/TableEntries.sql
+++ AuditDesk/Database/TableEntries.sql
@@ -2,3 +2,12 @@ insert into program
 values 
 	("UG-BTECH", "B.Tech.", "UG"),
   	("PG-MTECH", "M.Tech.", "PG");
+
+insert into department
+values 
+	(121, "CE"),
+    (122, "CSE"),
+    (123, "ENTC"),
+    (125, "ME"),
+    (126, "AIML"),
+    (127, "RNA");
