3568bc9fbc57ce380e5d0f5363e6a63f15120226
Dhairya Mehra <mpdhairyamehra000942@gmail.com>
2024-04-06 22:24:08 +0530

    Update TableEntries.sql

--- AuditDesk/Database/TableEntries.sql
+++ AuditDesk/Database/TableEntries.sql
@@ -17,3 +17,10 @@ values
     	(149, "AIML"),
     	(150, "ED"),
     	(152, "AT");
+
+insert into Faculty (Faculty_Name, Faculty_Email, Program_ID, password)
+values 
+	( "ABC", "abc@gmail.com", "UG-BTECH", "12345" ),
+    	("QWERTY", "qwerty@gmail.com", "PG-MTECH", "09876"),
+    	("ASDF", "asdf@fmail,com", "UG-BTECH", "635890"),
+    	("POIU","poiu@gmail.com", "PG-MTECH", "161104");
