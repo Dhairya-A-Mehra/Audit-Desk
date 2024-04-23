d4eff532d5d659c62456b3a2a6174622bc8d92eb
Rutu Bhanderi <140520663+rutubhanderi@users.noreply.github.com>
2024-04-24 00:01:53 +0530

    minor fixes
    added session fn to sign in
    improved db connection

---
+++ utils/Validations.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Rutu Bhanderi
+ */
+public class Validations {
+    // returns true if password is strong else false if password is weak
+    public static boolean isPasswordValid(String password) {
+
+        int len = password.length();
+
+        if (len < 8) {  // minimum password length requirement not met
+            return false;
+        } else {
+            // validation flags
+            boolean hasLower=false,
+                    hasUpper=false,
+                    hasDigit=false,
+                    hasSpChar=false;
+
+            Set<Character> spChars = new HashSet<>(
+                    Arrays.asList('!', '@', '#', '$', '%', '^', '&', '*'));
+
+            // checking all characters and setting necessary flags
+            for (char ch :password.toCharArray()) {
+                if (Character.isLowerCase(ch))
+                    hasLower = true;
+                if (Character.isUpperCase(ch))
+                    hasUpper = true;
+                if (Character.isDigit(ch))
+                    hasDigit = true;
+                if (spChars.contains(ch))
+                    hasSpChar = true;
+            }
+
+            // return true if all flags are true (i.e., all requirements are satisfied)
+            return hasLower && hasUpper && hasDigit && hasSpChar;
+        }
+    }
+    
+    // returns true if email is valid
+    public static boolean isEmailValid(String email) {
+        
+        int atIndex = email.indexOf('@');
+        int lastIndex = email.lastIndexOf('@');
+
+        // Check if "@" is present, occurs only once, and is not at first or last index
+        return atIndex != -1 && atIndex == lastIndex && atIndex != 0 && atIndex != email.length() - 1;
+    }
+}
+
+
+
+    
